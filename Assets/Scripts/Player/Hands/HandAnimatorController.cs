ad45367f67b6d29f8f27ea38bba634f8ee363ae5
RemoteShift <alloush-2004@hotmail.com>
2026-02-25 20:04:17 +0200

    Added Hand animations

---
+++ Assets/Scripts/Player/Hands/HandAnimatorController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimatorController : MonoBehaviour
+{
+    [Header("Input Action References")]
+    public InputActionReference triggerValue;
+    public InputActionReference gripValue;
+
+    private Animator handAnimator;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        handAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var trigger = triggerValue.action.ReadValue<float>();
+        var grip = gripValue.action.ReadValue<float>();
+        
+        handAnimator.SetFloat("Trigger", trigger);
+        handAnimator.SetFloat("Grip", grip);
+    }
+}
