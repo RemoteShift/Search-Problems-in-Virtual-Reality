27880150f38bfd005ca87e1ef353a72c671b7eae
RemoteShift <alloush-2004@hotmail.com>
2026-04-10 21:54:09 +0200

    Add ScriptableObject classes for search problems and types; implement SearchProblemSO and SearchProblemType

---
+++ Assets/Scriptable Objects/Search Problems/SearchProblemSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SearchProblemSO : ScriptableObject
+{
+        public string problemName;
+        public string description;
+        public SearchProblemType problemType;
+        public string[] actions;
+}
